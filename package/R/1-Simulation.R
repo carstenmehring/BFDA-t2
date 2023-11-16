6ec5e92148285ed96699ed610c1a8e72d9e1164a
Carsten Mehring <48163946+carstenmehring@users.noreply.github.com>
2023-11-16 11:09:18 +0100

    Update 1-Simulation.R

--- package/R/1-Simulation.R
+++ package/R/1-Simulation.R
@@ -141,8 +141,7 @@ BFDA.sim <- function(expected.ES, type=c("t.between", "t.paired", "correlation",
 					logBF	= logBF,
 					emp.ES	= freq.test$emp.ES,
 					statistic = freq.test$statistic,
-					p.value	= freq.test$p.value,
-					negBF   = 1)
+					p.value	= freq.test$p.value)
 
 				# if boundary is hit: stop sampling in this trajectory
 				if (logBF<=logBoundary[1] | logBF >= logBoundary[2]) {break;}
